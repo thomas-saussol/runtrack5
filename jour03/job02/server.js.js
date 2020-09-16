cb03b80acfecf1c1f92ab2211bddd8f430ba8ec3
thomas-saussol <thomas.saussol@orange.fr>
2020-09-16 14:28:29 +0200

    jusqu'au job4 finit

--- jour03/job02/server.js.js
+++ jour03/job02/server.js.js
@@ -1,13 +1,20 @@
 const http = require('http');
-
+const fs= require ('fs')
 const port = 3000;
 
 const server = http.createServer((req, res) => {
 
-    if(req.url=='/'){
-        res.write('Hello World!');
-        res.end();
-    }
+    fs.readFile('index.html', (err, data) => {
+        if (err) throw err;
+
+        if (req.url == '/') {
+            res.write('Hello World!');
+            res.end(data);
+        }
+        console.log();
+    });
+
+
 
 });
  server.listen(port);
