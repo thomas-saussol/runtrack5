c7def092c5d4bda420f77accab25a28440199470
thomas-saussol <thomas.saussol@orange.fr>
2020-09-18 11:17:47 +0200

    job 05

---
+++ jour05/job01/calculator9000/src/TheTitle.js
@@ -0,0 +1,20 @@
+
+
+import React, {Component} from 'react';
+
+class ResultComponent extends Component {
+
+
+    render() {
+        let {result} = this.props;
+        return (
+            <div className="result">
+                <h1>Calculator9000</h1>
+            </div>
+        )
+            ;
+    }
+}
+
+
+export default ResultComponent;
